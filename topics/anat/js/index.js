9711d8d43ffea2fcf917531fe0833ccc47b41b39
Alexander Berg <alexander.berg.p92@sonymusic.com>
2019-12-11 10:32:47 +0100

    added anatomy

---
+++ topics/anat/js/index.js
@@ -0,0 +1,184 @@
+var questionAnswers = [
+// Teszt
+{
+	question: '1. Hámszövet',
+	answer: '0B4mCaX952KoaUDFENlp1eGpENTA'
+},{
+	question: '2. Kötőszövet',
+	answer: '0B4mCaX952KoaX0V6bF9TQldQRjQ'
+},{
+	question: '3. Zsírszövet; Porcszövet',
+	answer: '0B4mCaX952KoadXAzdzMwWktJX2c'
+},{
+	question: '4. Csontszövet',
+	answer: '0B4mCaX952KoaM3FkMF9QUlB4UUE'
+},{
+	question: '5. Izomszövet',
+	answer: '0B4mCaX952KoaTU5jUHItb2N2SW8'
+},{
+	question: '6. Az ivarsejtek kialakulása, Ivarsejtek; Megtermékenyítés;',
+	answer: '0B4mCaX952KoaemZDWXp1U3NFcFE'
+},{
+	question: '7. A magzat kialakulása, A magzat fejlődése',
+	answer: '0B4mCaX952KoaeXNnaVBKQlZzS0U'
+},{
+	question: '8. Extrauterin fejlődés',
+	answer: '0B4mCaX952KoaZTVQRGlwSjJoS0U'
+},{
+	question: '9. Csontokról általában; A csontok szerkezete, fejlődése, növekedése',
+	answer: '0B4mCaX952KoaMHVNODVJZWtybVk'
+},{
+	question: '10. A csontok járulékos alkotórészei; A csontok összeköttetései',
+	answer: '0B4mCaX952Koad3NnVzkzZm5tTTQ'
+},{
+	question: '11. Az arckoponya',
+	answer: '0B4mCaX952KoaZDRMRVR6enNYdEU'
+},{
+	question: '12. Az agykoponya',
+	answer: '0B4mCaX952KoaOTJUYzBfUVBIZjA'
+},{
+	question: '13. A koponyatető és a koponyaalap',
+	answer: '0B4mCaX952KoaNVlTdlZDZTBjb2c'
+},{
+	question: '14. A koponya üregei',
+	answer: '0B4mCaX952KoaRmwxZWdxa1ZsdTA'
+},{
+	question: '15. A mandibula és ízülete',
+	answer: '0B4mCaX952KoabS1xdW01dkcwUlU'
+},{
+	question: '16. A sternum, a bordák, ezek összeköttetései; A mellkas',
+	answer: '0B4mCaX952KoaQ3FZcndlTlhKYlU'
+},{
+	question: '17. A csigolyák és a gerinc',
+	answer: '0B4mCaX952KoaSWR0QnFNc09WRVE'
+},{
+	question: '18. A vállöv csontjai és ízületei',
+	answer: '0B4mCaX952KoacFRDU29YNjVXQ1E'
+},{
+	question: '19. Humerus; Vállízület',
+	answer: '0B4mCaX952KoaWXdac2tVQnVDRkU'
+},{
+	question: '20. Az alkar csontjai; A könyökízület',
+	answer: '0B4mCaX952KoaNl85YngxLUlOZVU'
+},{
+	question: '21. A kéz csontjai és ízületei',
+	answer: '0B4mCaX952KoabHF3NzdzWXRnVDQ'
+},{
+	question: '22. A medencecsont és összeköttetései',
+	answer: '0B4mCaX952KoaUmdmbFBaVEhwZVU'
+},{
+	question: '23. A medence egészben',
+	answer: '0B4mCaX952KoaUnd1UDFJcnpWREE'
+},{
+	question: '24. A combcsont és a csípőízület',
+	answer: '0B4mCaX952KoaRngtbDRjQkp2NkU'
+},{
+	question: '25. A lábszár csontjai és a térdízület',
+	answer: '0B4mCaX952KoaZlZObEpMaVVxR2s'
+},{
+	question: '26. A láb csontjai és ízületei; A láb egészben',
+	answer: '0B4mCaX952KoaSzVaa25EQjduR2c'
+},{
+	question: '27. A vázizmokról általában; Az izom szerkezete, felépítése, összetétele',
+	answer: '0B4mCaX952KoaSU1KN1pGRFB1aFk'
+},{
+	question: '28. A fejizmok',
+	answer: '0B4mCaX952KoaeGZfT3RrNWpXR28'
+},{
+	question: '29. A mellkas izmai',
+	answer: '0B4mCaX952KoaQVBrcUh2a1JNaG8'
+},{
+	question: '30. A hátizmok',
+	answer: '0B4mCaX952KoaeEM3TkhQYlg1S1E'
+},{
+	question: '31. A nyakizmok',
+	answer: '0B4mCaX952KoaMFh0dEUxSlNiZlU'
+},{
+	question: '32. A hasizmok',
+	answer: '0B4mCaX952Koad0NNMjJFZTV1WTg'
+},{
+	question: '33. A sérvcsatornák',
+	answer: '0B4mCaX952KoaN0tsU2JvSEVIUzA'
+},{
+	question: '34. A felsővégtag izmai',
+	answer: '0B4mCaX952KoabzFobWM4OHhCME0'
+},{
+	question: '35. Az alsó végtag izmai',
+	answer: '0B4mCaX952KoaSkxIcjZ5ZmdnQUE'
+},{
+	question: '36. A szív külső leírása; helyzete, vetülete',
+	answer: '0B4mCaX952Koaemp4a1dQQXBjczQ'
+},{
+	question: '37. A szív üregei, szájadékai, billentyűi',
+	answer: '0B4mCaX952KoaenM1anZjWmt0V3c'
+},{
+	question: '38. A szívfal szerkezete és a szívburok',
+	answer: '0B4mCaX952KoaS1ZuZXhDVklfcU0'
+},{
+	question: '39. A szív beidegzése',
+	answer: '0B4mCaX952KoaZm9vdG1IZWVsN2s'
+},{
+	question: '40. A szív saját erei',
+	answer: '0B4mCaX952KoaQzhzUEJQcU9WeGc'
+},{
+	question: '41. Az erekről általában, az erek szerkezete; összeköttetései ; Vérkörök',
+	answer: '0B4mCaX952KoaNDJXX3h4dlo3eDQ'
+},{
+	question: '42. Az aorta ascendens, az arcus aortae és ágaik',
+	answer: '0B4mCaX952KoaZWZUSmVtejRJNkk'
+},{
+	question: '43. Az aorta descendens és ágai',
+	answer: '0B4mCaX952KoaMUdfdVU5Z2pjNGc'
+},{
+	question: '44. A nagyvérkör vénái',
+	answer: '0B4mCaX952KoaOTZUYUlsRHlJZlk'
+},{
+	question: '45. A magzati vérkeringés',
+	answer: '0B4mCaX952Koac1JUQlpaM3hhZnc'
+},{
+	question: '46. A nyirokrendszer, nyirokszervek, nyirokkeringés',
+	answer: '0B4mCaX952KoaS3pQN05JRm54R3c'
+},{
+	question: '47. A nyirokcsomó és a lép',
+	answer: '0B4mCaX952KoaQW5oUFo3R21XbzQ'
+},{
+	question: '48. A mandulák és a csecsemőmirigy',
+	answer: '0B4mCaX952KoaU05IUTdYWnNBMmc'
+},{
+	question: '49. A vér és alakos elemei, vérplazma',
+	answer: '0B4mCaX952KoaemFWemNvQnBrYmc'
+},{
+	question: '50. A csontvelő és a vérképzés; A vörösvértestek és a fehérvérsejtek fejlődése',
+	answer: '0B4mCaX952KoaSmhuTml5dWdhdkU'
+}
+];
+
+function init() {
+	const list = document.getElementById('questions'); 
+	
+	questionAnswers.forEach(item => {
+		let option = document.createElement('option');
+		option.value = item.question;
+		list.appendChild(option);
+	});
+};
+
+function searchAnswer() {
+	var documentFrame = document.getElementById('documentFrame');
+	var question = document.getElementById('question').value;
+	
+	questionAnswers.forEach(item => {
+		if (item.question == question) {		
+			documentFrame.src = "https://docs.google.com/viewer?srcid=" + item.answer + "&pid=explorer&efh=false&a=v&chrome=false&embedded=true";
+			documentFrame.style.height='100%';
+			documentFrame.style.visibility = null;		
+		}
+	});
+}
+
+function reset() {
+	document.getElementById('question').value = '';
+	var documentFrame = document.getElementById('documentFrame');
+	documentFrame.style.height=0;
+	documentFrame.style.visibility = 'hidden';
+}
