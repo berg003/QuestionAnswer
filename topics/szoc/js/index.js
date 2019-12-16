21ee18ddb41d8a4e45efe9bb9b99f58eefafc8a4
Alexander Berg <alexander.berg.p92@sonymusic.com>
2019-12-16 12:45:54 +0100

    changed icons added some answrs to szoc

--- topics/szoc/js/index.js
+++ topics/szoc/js/index.js
@@ -1,8 +1,26 @@
 var questionAnswers = [
 // Teszt
 {
-	question: 'Tesztecske?',
-	answer: 'Tesztecske'
+	question: '9. Mit tartalmaz David Armstrong pszichoanalitikai egészségértelmezési modellje??',
+	answer: '-A betegségek pszichogenetikai sajátosságaira koncentrál</br>- Jellemzően az egyéni késztetések, szükségletek és ösztönök, illetve ezek társadalmi kontrolljának és korlátainak konfliktusát helyezi előtérbe. (Tudattalanban zajlanak.)'
+},{
+	question: '10. Mit tartalmaz David Armstrong magatartáselméleti egészségértelmezési modellje?',
+	answer: '- Bizonyos magatartási modellek (hiperaktivitás, mértéktelen alkoholfogyasztás, figyelemzavar) önmagukban is betegségként érzékelhetők</br>- Előidéznek, hozzájárulnak betegségek kialakulásához: kockázati magatartások, veszélyeztető viselkedések, káros cselekedetek.'
+},{
+	question: '11. Mit tartalmaz David Armstrong szociológiai egészségértelmezési modellje?',
+	answer: '- Emberi szervezet szomatikus/pszichés elváltozásai milyen társadalmi reagálási módokhoz vezetnek, egyén-társadalom részéről</br>- Megbetegedett egyén identitása megváltozik (cselekvőképessége is), társadalom is máshogy viszonyul hozzá: betegnek tekinti, újszerű kötelességeket, jogokat rendel hozzá.'
+},{
+	question: '12. Mit tartalmaz David Armstrong biomedicinális egészségértelmezési modellje?',
+	answer: '- Emberi szervezet szervstruktúrájából indul ki</br>- Anat: ép és funkcionális szervstr.-t tekinti egészségesnek. Szerveknek, sejteknek elváltozásait pedig betegségnek.'
+},{
+	question: '13. Mit fejez ki Ivan Illich kulturális iatrogenezis-elmélete?',
+	answer: '- Medicina növekvő befolyása</br>- Megfosztja az embereket attól, hogy megküzdjenek az életük és testük természetes változásaival (halállal, betegséggel, fájdalommal).</br>- Abnormálisnak tekinti: melyek elkerülhetők, elkerülendők.</br>- Modern orvoslás: segítséget nyújt a szenvedés ellen.'
+},{
+	question: '14. Mit fejez ki Ivan Illich klinikai iatrogenezis-elmélete?',
+	answer: '- Orvoslás által alkalmazott eljárások mellékhatásai olyan betegség, amelyeket a gyógyszer, orvosi döntés, kórházi tartózkodás okoz.</br>- Egy terápia rosszabb következményekkel jár, mint a gyógyítani kívánt betegség.'
+},{
+	question: '15. Mit fejez ki Ivan Illich társadalmi iatrogenezis-elmélete?',
+	answer: '- Biomedicina szélesebb társadalomra is kiterjeszti hatáskörét.</br>- Eredmény: passzív, engedelmes társadalom.</br>- Az ember függ: gyógyszertől, orvosi magyarázattól. </br>- Minden szenvedés kórházban kezelnek.</br>- Otthon többé nem alkalmas betegséghez, szüléshez, halálhoz.'
 }
 ];
 
