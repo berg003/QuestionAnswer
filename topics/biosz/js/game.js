9606607078cd7794988b033efbe5fae93e8861b0
Alexander Berg <alexander.berg.p92@sonymusic.com>
2019-12-17 23:31:23 +0100

    added q/a game to biosz

--- topics/biosz/js/game.js
+++ topics/biosz/js/game.js
@@ -255,6 +255,390 @@ var questionAnswers=[
 			'A nukleotidok felépítésében nem vesz részt': false,
 			'Ribóz vagy dezoxiribóz vesz részt a felépítésében' : false
 		}
+	},{
+		question:'Jelölje meg a sejtekre igaz állításokat!',
+		answers: {
+			'Létezésüket Robert Hook figyelte meg először': true,
+			'Mindig sejtfal határolja el őket a környezetüktől': false,
+			'Valamennyi életjelenség végzésére képesek': true,
+			'Nagyobb állatok sejtjei nagyobbak': false
+		}
+	},{
+		question:'Az intermedier anyagcsere folyamatai közé tartozik:',
+		answers: {
+			'Sejten belüli szintetikus folyamatok': true,
+			'Tápanyagok megemésztése a tápcstornában': false,
+			'Sejten belüli disszimilációs folyamatok': true,
+			'A monomerek eljuttatása a sejtekig': false
+		}
+	},{
+		question:'Az alábbi állítások közül az adenozin-trifoszfát (ATP) molekulákra igaz, hogy',
+		answers: {
+			'Főleg a felépítő folyamatokban keletkeznek': false,
+			'A felépítő folyamatok általános „fizetőeszközei”': true,
+			'A lebontó folyamatok sohasem használnak el belőle': false,
+			'A lebontó folyamatok során felszabaduló energia tárolói': true
+		}
+	},{
+		question:'Mit nevezünk plazmamembránnak?',
+		answers: {
+			'Az intracelluláris membránok összességét': false,
+			'Az intercelluláris membránok összességét': false,
+			'A sejtek különféle membránjait összefoglaló néven': false,
+			'A sejtek külső, elhatároló membránját': true 
+		}
+	},{
+		question:'A biológiai membránok felépítésére jellemző, hogy',
+		answers: {
+			'Kettős fehérjeréteg között egy lipidréteg húzódik': false,
+			'Két lipidréteg között egy fehérjeréteget találunk': false,
+			'A különféle lipidekből álló területek mozaikszerűen váltják egymást': false,
+			'A lipid kettősrétegbe  mozaikszerűen fehérjék ágyazódnak be': true
+		}
+	},{
+		question:'Melyik a legegyszerűbb foszfolipid neve?',
+		answers: {
+			'Kolin': false,
+			'Foszfatidsav': true,
+			'Szerin': false,
+			'Etanolamin': false
+		}
+	},{
+		question:'Válassza ki a transzmembrán fehérjékre igaz állításokat!',
+		answers: {
+			'Perifériás membránproteinek': false,
+			'Átérik a lipid kettősréteget': true,
+			'Csak átmenetileg tartózkodnak a membránban': false,
+			'Közéjük tartoznak a csatornafehérjék': true
+		}
+	},{
+		question:'Hol található a glikokálix?',
+		answers: {
+			'Májsejtek raktározó granulumaiban': false,
+			'A növényi sejtfalban': false,
+			'Az agykéreg bizonyos területein': false,
+			'A sejtfelszínen': true
+		}
+	},{
+		question:'Mi az adhéziós fehérje szerepe egy sejtkapcsoló struktúrában?',
+		answers: {
+			'Összeköti a plazmamembránt a sejtfallal': false,
+			'Elhatárolja a sejtet a tapadási felszíntől': false,
+			'Megköti a később felszívásra kerülő anyagokat': false,
+			'Összeköti a sejten belüli és a sejten kívüli struktúrákat': true
+		}
+	},{
+		question:'Jelölje meg a kommunikáló sejtkapcsolódást!',
+		answers: {
+			'Záróléc': false,
+			'Foltdezmoszóma': false,
+			'Övdezmoszóma': false,
+			'Réskapcsolat': true
+		}
+	},{
+		question:'A passzív transzport során az anyagáramlás iránya mindig a',
+		answers: {
+			'Nagyobb koncentrációjú helyről a kisebb koncentrációjú felé történik': true,
+			'Kisebb koncentrációjú helyről a nagyobb koncentrációjú felé történik': false,
+			'Koncentrációkülönbségek megszüntetését célozza': true,
+			'Mindenkori ozmózisnyomásnak megfelelő': false
+		}
+	},{
+		question:'Milyen funkció ellátására alakultak ki a mikrobolyhok?',
+		answers: {
+			'Vízvisszaszívás': false,
+			'Tápanyagfelszívás': true,
+			'Anyagkeverés': false,
+			'Kiválasztás': false
+		}
+	},{
+		question:'A sejtek melyik részén találjuk általában a mikrobolyhokat?',
+		answers: {
+			'A laterális felszíneken': false,
+			'Az apikális felszínen': true,
+			'A bazális felszíne': false,
+			'A sejt teljes felületén': false
+		}
+	},{
+		question:'Milyen funkció ellátására alakult ki a bazális csíkolat?',
+		answers: {
+			'Tápanyagfelszívás': false,
+			'Anyagkeverés': false,
+			'Kiválasztás': false,
+			'Folyadékvisszaszívás': true
+		}
+	},{
+		question:'Mit találunk a maghártya citoplazma felöli oldalán?',
+		answers: {
+			'Riboszómákat': true,
+			'Lamina fibrosát': false,
+			'Kariolimfát': false,
+			'Heterokromatint': false
+		}
+	},{
+		question:'Hol található a perinukleáris tér?',
+		answers: {
+			'A sejtmag körül': false,
+			'A sejtmagvacska körül': false,
+			'A külső és a belső maghártya között': true,
+			'Radioaktív izotópok környezetében': false
+		}
+	},{
+		question:'Mit értünk humán genom alatt?',
+		answers: {
+			'Az emberi sejtek sejtmagjában megtalálható teljes DNS-állományt': true,
+			'Az emberi sejtek teljes DNS tartalmát': false,
+			'Az emberi 26 pár kromoszóma DNS tartalmát': false,
+			'Az emberi ivarsejtek összességét': false
+		}
+	},{
+		question:'Mit értünk az alatt, hogy a DNS replikáció mechanizmusa szemikonzervatív?',
+		answers: {
+			'S. Watson és Crick elmélete a replikációról csak félig igazolódott be': false,
+			'Félig a kiindulási sejtben játszódik le, félig az utódsejtben': false,
+			'A másolás csak félig-meddig pontos': false,
+			'Az utód kettős hélixekben az egyik szál mindig az eredeti': true
+		}
+	},{
+		question:'Válassza ki a replikáció mechanizmusának alapjául szolgáló DNS jellemzőt!',
+		answers: {
+			'A szemben lévő bázisok szigorú komplementaritása': true,
+			'A cukor-foszfát lánc antiparallel lefutása': false,
+			'A templát lánc mindig helikális lefutású': false,
+			'A szintézis mindkét oldalon folyamatos': false
+		}
+	},{
+		question:'A sejt életének mely szakaszában zajlik a DNS replikációja?',
+		answers: {
+			'A sejtosztódás metafázisában': false,
+			'Amikor a kromatidák szétosztódnak az utódsejtekbe': false,
+			'Az interfázis S szakaszában': true,
+			'Amikor a DNS-ben tárolt információ RNS-be íródik': false
+		}
+	},{
+		question:'Mi akadályozza meg a replikáció során azt, hogy az eredeti DNS láncok visszazáródjanak?',
+		answers: {
+			'Helikáz': false,
+			'ssb fehérjék': true,
+			'Okazaki-fragmentumok': false,
+			'Primer': false
+		}
+	},{
+		question:'Melyik enzim végzi az Okazaki-fragmentumok összekapcsolását?',
+		answers: {
+			'Helikáz': false,
+			'Primáz': false,
+			'Ligáz': true,
+			'Polimeráz': false
+		}
+	},{
+		question:'Mi a polimeráz enzimek szerepe a DNS-replikációja során?',
+		answers: {
+			'Az RNS nukleotidok beépítése a mRNS-be a DNS templát alapján': false,
+			'Az új láncok templáttal komplementer nukleotidjait építik be': true,
+			'A primer megszintetizálása a DNS templáton': false,
+			'Az Okazaki-fragmentumok folyamatos lánccá alakítása': false
+		}
+	},{
+		question:'A kromatin állomány mely részén történik a DNS replikáció?',
+		answers: {
+			'Az eukrometinban': false,
+			'A konstitutív heterokromatinban': false,
+			'A fakultatív heterokromatinban': false,
+			'A teljes területén végbemegy': true
+		}
+	},{
+		question:'Mi a primáz enzim szerepe?',
+		answers: {
+			'Egy rövid komplementer RNS szakaszt szintetizál': true,
+			'Egy rövid komplementer DNS szakaszt szintetizál': false,
+			'Összekapcsolja az Okazaki-fragmentumokat': false,
+			'Nyitva tartja a replikációs villát': false
+		}
+	},{
+		question:'Mit nevezünk transzkripciónak?',
+		answers: {
+			'DNS templáton végrehajtott DNS-szintézist': false,
+			'RNS templáton végrehajtott RNS-szintézist': false,
+			'RNS templáton végrehajtott DNS-szintézist': false,
+			'DNS templáton végrehajtott RNS-szintézist': true
+		}
+	},{
+		question:'Jelölje meg a sejtekre igaz állításokat!',
+		answers: {
+			'Létezésüket Robert Hook figyelte meg először': true,
+			'Mindig sejtfal határolja el őket a környezetüktől': false,
+			'Valamennyi életjelenség végzésére képesek': true,
+			'Nagyobb állatok sejtjei nagyobbak': false
+		}
+	},{
+		question:'Mit nevezünk plazmamembránnak?',
+		answers: {
+			'Az intracelluláris membránok összességét': false,
+			'Az intercelluláris membránok összességét': false,
+			'A sejtek különféle membránjait összefoglaló néven': false,
+			'A sejtek külső, alhatároló membránját': true
+		}
+	},{
+		question:'A biológiai membránok felépítésére jellemző, hogy',
+		answers: {
+			'Kettős fehérjeréteg között egy lipidréteg húzódik': false,
+			'Két lipidréteg között egy fehérjeréteget találunk': false,
+			'A különféle lipidekből álló területek mozaikszerűen váltják egymást': false,
+			'A lipid kettősrétegbe  mozaikszerűen fehérjék ágyazódnak be': true
+		}
+	},{
+		question:'A foszfolipidek amfipatikus jellege abból adódik, hogy',
+		answers: {
+			'A glicerin harmadik hidroxilcsoportját egy poláros jellegű csoport észteresíti': true,
+			'A glicerin mindhárom hidroxilcsoportját egy zsírsavlánc észteresíti': false,
+			'Hidrofób és hidrofil molekularészleteket is találunk bennük': true,
+			'Poláros csoportjaik körül a vízmolekulák hidrátburkot alakítanak ki': false
+		}
+	},{
+		question:'Milyen kötőerők működnek a biológiai membránokat felépítő foszfolipidek között?',
+		answers: {
+			'Kovalens kötések': false,
+			'Ionos kötőerők': false,
+			'Van der Waals kölcsönhatások': true,
+			'Hidrogén-hidak': true
+		}
+	},{
+		question:'Válassza ki a glikoproteinekre igaz állításokat!',
+		answers: {
+			'Sejtfelszíni receptorként működhetnek': true,
+			'A membrán extracelluláris rétegében több van belőlük': true,
+			'Sejtfelszíni markerként működhetnek': true,
+			'A májsejtekben tárolt tartaléktápanyag': false
+		}
+	},{
+		question:'Hol található a glikokálix?',
+		answers: {
+			'Májsejtek raktározó granulumaiban': false,
+			'A növényi sejtfalban</br>': false,
+			'Az agykéreg bizonyos területein': false,
+			'A sejtfelszínen': true
+		}
+	},{
+		question:'Jelölje meg azt a sejtkapcsolódást, melyben direkt módon nem vesznek részt adhéziós fehérjék!',
+		answers: {
+			'Interdigitáció': true,
+			'Övdezmoszóma': false,
+			'Réskapcsolat': false,
+			'Záróléc': false
+		}
+	},{
+		question:'Jelölje meg azokat a forrásokat, melyek a membrántranszportfolyamatok hajtóerejéül szolgálhatnak, energiaigényét fedezhetik!',
+		answers: {
+			'Koncentrációkülönbség a membrán két oldala között': true,
+			'Szintetikus folyamatokban keletkező energia': false,
+			'Potenciálkülönbség a membrán két oldala között': true,
+			'Anabolikus folyamatok során felszabaduló energia': false
+		}
+	},{
+		question:'Milyen funkció ellátására alakultak ki a mikrobolyhok?',
+		answers: {
+			'Vízvisszaszívás': false,
+			'Tápanyagfelszívás': true,
+			'Anyagkeverés': false,
+			'Kiválasztás': false
+		}
+	},{
+		question:'A sejtek melyik részén találjuk általában a mikrobolyhokat?',
+		answers: {
+			'A laterális felszíneken': false,
+			'Az apikális felszínen': true,
+			'A bazális felszínen': false,
+			'A sejt teljes felületén': false
+		}
+	},{
+		question:'A sejtek melyik részén találjuk általában a bazális csíkolatot?',
+		answers: {
+			'Az oldalsó felszínen': false,
+			'Az alapi felszínen': true,
+			'A csúcsi felszínen': false,
+			'A sejt teljes felületén': false
+		}
+	},{
+		question:'Milyen funkció ellátására alakult ki a bazális csíkolat?',
+		answers: {
+			'Tápanyagfelszívás': false,
+			'Anyagkeverés': false,
+			'Kiválasztás': false,
+			'Folyadékvisszaszívás': true
+		}
+	},{
+		question:'Jelölje meg a sejtváz feladatait!',
+		answers: {
+			'Sejten belüli mozgások bonyolítása': true,
+			'Sejt egészének mozgatása': true,
+			'Sejt alakjának biztosítása': true,
+			'Tápanyagok raktározása': false
+		}
+	},{
+		question:'Az alábbi állítások közül válassza ki azt, amelyik a sejtmaghártyára igaz!',
+		answers: {
+			'Egy foszfolipid kettősrétegből áll': false,
+			'Két foszfolipid kettősrétegből áll': true,
+			'A Golgi-apparátussal összefügg': false,
+			'A durva felszínű endoplazmatikus retikulummal összefügg': true
+		}
+	},{
+		question:'Hol található a perinukleáris tér',
+		answers: {
+			'A sejtmag körül': ,false
+			'A sejtmagvacska körül': false,
+			'A külső és a belső maghártya között': true,
+			'Radioaktív izotópok környezetében': false
+		}
+	},{
+		question:'Milyen két területeket kötnek össze a magpórusok',
+		answers: {
+			'A citoplazma és a sejtmag területét': true,
+			'A perinukleáris teret a citoplazmával': false,
+			'A perinukleáris teret a kromatinállománnyal': false,
+			'A magvacskát a kromatin állomány más területeivel': false
+		}
+	},{
+		question:'Mit értünk humán genom alatt?',
+		answers: {
+			'Az emberi sejtek sejtmagjában megtalálható teljes DNS-állományt': true,
+			'Az emberi sejtek teljes DNS tartalmát': false,
+			'Az emberi 23 pár kromoszóma DNS tartalmát': true,
+			'Az emberi ivarsejtek összességét': false
+		}
+	},{
+		question:'',
+		answers: {
+			'': ,
+			'': ,
+			'': ,
+			'': 
+		}
+	},{
+		question:'',
+		answers: {
+			'': ,
+			'': ,
+			'': ,
+			'': 
+		}
+	},{
+		question:'',
+		answers: {
+			'': ,
+			'': ,
+			'': ,
+			'': 
+		}
+	},{
+		question:'',
+		answers: {
+			'': ,
+			'': ,
+			'': ,
+			'': 
+		}
 	}
 ]
 
